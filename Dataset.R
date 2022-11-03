63ecd3ff39a50543712f7794b7639ac2ddd14ccd
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-11-03 11:10:02 +0000

    Implement column exists check

--- Dataset.R
+++ Dataset.R
@@ -4,4 +4,23 @@ dataset <- setRefClass("Dataset", fields = list(
                                     df = "data.frame",
                                     response_variable_column = "character",
                                     to_binary_column = "character"),
-                                  methods = list())
+                                  methods = list(
+                                    # check_column_exists
+                                    check_column_exists = function(df, column) {
+                                      if (!(column %in% names(df))) {
+                                        stop(sprintf("Column '%s' is not part of the supplied dataframe.", column))
+                                      }
+                                    },
+                                    
+                                    # constructor
+                                    initialize = function(df, response_variable_column, to_binary_column) {
+                                      # We check for the response variable and to binary columns whether they are present
+                                      check_column_exists(df, response_variable_column)
+                                      check_column_exists(df, to_binary_column)
+                                      
+                                      # Checks OK, pass data to weird R constructor
+                                      callSuper(df=df,
+                                                response_variable_column=response_variable_column,
+                                                to_binary_column=to_binary_column)
+                                    }
+                                  ))
