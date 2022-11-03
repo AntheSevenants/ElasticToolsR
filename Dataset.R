9febd85f3f30bd98f7b8e025fe1f2ab75c08b34d
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-11-03 11:30:58 +0000

    Implement column binary check

--- Dataset.R
+++ Dataset.R
@@ -13,10 +13,17 @@ dataset <- setRefClass("Dataset", fields = list(
                                       }
                                     },
                                     
-                                    # chech_column_is_factor
+                                    # check_column_is_factor
                                     check_column_is_factor = function(df, column) {
                                       if (!is.factor(df[[column]])) {
                                         stop(sprintf("Column '%s' should be of type 'factor'", column))
+                                      }
+                                    },
+                                    
+                                    # check_column_is_binary
+                                    check_column_is_binary = function(df, column) {
+                                      if (length(unique(df[[column]])) != 2) {
+                                        stop(sprintf("Column '%s' should only contain two unique values", column))
                                       }
                                     },
                                     
@@ -29,6 +36,9 @@ dataset <- setRefClass("Dataset", fields = list(
                                       # We check for the 'response variable' and 'to binary' columns whether they are factors
                                       check_column_is_factor(df, response_variable_column)
                                       check_column_is_factor(df, to_binary_column)
+                                      
+                                      # We check whether the response variable column is binary
+                                      check_column_is_binary(df, response_variable_column)
                                       
                                       # Save the context features
                                       context_features <<- unique(as.character(df[[to_binary_column]]))
