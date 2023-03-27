026ec51a214fd30568d6dc8baf41fb9bdd98d0b2
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-03-27 14:26:26 +0200

    Move to recommended alpha finding method

--- ElasticNet.R
+++ ElasticNet.R
@@ -1,6 +1,7 @@
 library(methods)
 library(glmnet)
 library(parallel)
+library(doMC)
 library(Matrix)
 
 # We'll need this
@@ -10,70 +11,41 @@ logit2p <- function(logit){
   return(prob)
 }
 
-if (Sys.info()['sysname'] == "Windows") {
-  numCores <- 1
-} else {
+enable_parallel_processing <- function(numCores) {
+
   numCores <- detectCores()
-  numCores
+  # Then, register the multicore worker
+  registerDoMC(cores = numCores)
 }
 
+enable_parallel_processing()
 
 elastic_net <- setRefClass("ElasticNet", fields = list(
                                          ds = "Dataset",
-                                         train_share = "numeric",
-                                         y.test = "numeric",
-                                         y.train = "numeric",
-                                         x.test = "dgCMatrix",
-                                         x.train = "dgCMatrix"),
+                                         y = "numeric",
+                                         x = "dgCMatrix",
+                                         type.measure = "character"),
                                   methods = list(
-                                    initialize = function(ds, train_share=0.7) {
+                                    initialize = function(ds, nfolds=10,
+                                                          type.measure="deviance") {
                                       ds <<- ds
                                       
-                                      sparse_coords <- ds$get_sparse_coords()
+                                      x <<- ds$as_matrix()
+                                      y <<- ds$response_variables
+                                      type.measure <<- type.measure
                                       
-                                      # Generate internal ids -> used for randomisation
-                                      # Attach response variables
-                                      df_shim <- data.frame("id" = 1:nrow(ds$df),
-                                                            "response" = ds$response_variables)
-                                      
-                                      # Split the dataset into testing and training
-                                      in_sample <- sample(c(TRUE, FALSE),
-                                                           nrow(df_shim),
-                                                           replace=TRUE,
-                                                           prob=c(train_share, 1 - train_share))
-                                      df.train <- df_shim[in_sample, ]
-                                      df.test  <- df_shim[!in_sample, ]
-                                      
-                                      # Get the response variables from both datasets
-                                      y.train <<- df.train$'response'
-                                      y.test <<- df.test$'response'
-                                      
-                                      # Now, we look at the df.train and df.test data frames
-                                      # We only retrain the rows from the sparse coordinates 
-                                      # for which the x column values are part of the data frame
-                                      x.train_ <- subset(sparse_coords,
-                                                        `x` %in% df.train$`id`)
-                                      x.test_ <- subset(sparse_coords,
-                                                       `x` %in% df.test$`id`)
-                                      
-                                      # Then, convert the data frames to actual sparse matrices
-                                      x.train <<- sparseMatrix(x.train_$x, x.train_$y, x=x.train_$value)
-                                      x.test <<- sparseMatrix(x.test_$x, x.test_$y, x=x.test_$value)
-                                      
-                                      # Remove empty rows
-                                      x.train <<- x.train[tabulate(summary(x.train)$i) > 0, , drop = FALSE]
-                                      x.test <<- x.test[tabulate(summary(x.test)$i) > 0, , drop = FALSE]
-                                      
-                                      # Simple and easy!
-                                      
-                                      train_share <<- train_share
+                                      ds$df$'foldid' <<- sample(1:nfolds,
+                                                            size = length(y),
+                                                            replace = TRUE)
                                     },
                                     do_elastic_net_regression = function(alpha=0.5) {
-                                      fit <- cv.glmnet(x.train,
-                                                       y.train,
-                                                       type.measure="deviance",
+                                      fit <- cv.glmnet(x,
+                                                       y,
+                                                       type.measure=type.measure,
                                                        alpha=alpha,
-                                                       family="binomial")
+                                                       family="binomial",
+                                                       foldid=ds$df$'foldid',
+                                                       parallel=TRUE)
                                       
                                       return(fit)
                                     },
@@ -83,12 +55,11 @@ elastic_net <- setRefClass("ElasticNet", fields = list(
                                     do_lasso_regression = function() {
                                       return(do_elastic_net_regression(alpha=1))
                                     },
-                                    do_cross_validation = function(k=10, cores_to_use=NA) {
+                                    do_cross_validation = function(k=10) {
                                       # Define the range of possible ks
                                       alpha_values <- lapply(0:k, function(i) { return(i/k) })
                                       alpha_values <- as.vector(alpha_values)
-                                      # Pre-generate all regression fit names
-                                      fit_names <- lapply(alpha_values, function(alpha) { return(paste0("alpha", alpha)) })
+
                                       # Actually compute the regression fits
                                       # We test k times, each time with a different alpha value
                                       # We save all results and then decide which model produces the best fit
@@ -97,46 +68,41 @@ elastic_net <- setRefClass("ElasticNet", fields = list(
                                       # We give the parameter to our internal elastic net function
                                       # This will compute a model for this alpha value
                                       # The fit is saved in our list of fits
+                                      regression_fits <- lapply(alpha_values,
+                                        function(alpha) { 
+                                          print(paste("Calculating for alpha =", alpha))
+                                          return(do_elastic_net_regression(alpha=alpha)) })
                                       
-                                      # If the feature matrix is very large, we get OOM issues
-                                      # So, decide on cores dynamically
-                                      if (is.na(cores_to_use)) {
-                                        cores_to_use <- numCores
-                                        if (dim(x.train)[1] > 5000) {
-                                          cores_to_use <- max(1, numCores / 2)
-                                        }
-                                      }
+                                      # For each fit, we check get the index of the 1se lambda
+                                      # It's a good practice to pick this lambda
+                                      l1se_indices <- sapply(regression_fits,
+                                          function(fit) { fit$index["1se",] })
                                       
-                                      regression_fits <- mclapply(alpha_values,
-                                                                  function(alpha) { 
-                                                                    return(do_elastic_net_regression(alpha=alpha)) },
-                                                                  mc.cores = cores_to_use)
+                                      # Then also collect all losses and other information
+                                      losses <- sapply(1:length(alpha_values),
+                                        function(index) {
+                                          l1se_index = l1se_indices[[index]]
+                                          return(regression_fits[[index]]$cvm[[l1se_index]])
+                                      })
                                       
-                                      # Now we want to find out which model actually performs the best
-                                      # To do this, we ask the model to predict the values given the predictors
-                                      # Then, we choose the model which gives the best predictions
-                                      losses <- lapply(1:length(alpha_values), function(i) {
-                                        test.predict <- predict(regression_fits[[i]],
-                                                                s=regression_fits[[i]]$lambda.1se,
-                                                                newx=x.test)
-                                        
-                                        # Convert logits to probabilities
-                                        test.predict <- logit2p(test.predict)
-                                        
-                                        # I decided to use cross entropy loss, since we're using a binary
-                                        # classification task. Thanks to Sien Moens for teaching me this 
-                                        # in the NLP course!
-                                        loss <- sum(-(y.test * log(test.predict)))
-                                        
-                                        return(loss)
+                                      nzeroes <- sapply(1:length(alpha_values),
+                                        function(index) {
+                                          l1se_index = l1se_indices[[index]]
+                                          return(regression_fits[[index]]$nzero[[l1se_index]])
+                                      })
+                                      
+                                      lambdas <- sapply(regression_fits,
+                                        function(fit) {
+                                          return(fit$lambda.1se)
                                       })
                                       
                                       # Create a data frame
                                       results <- data.frame(
-                                        `_id` = 1:length(fit_names),
-                                        alpha_value=as.double(alpha_values),
-                                        fit_name=as.character(fit_names),
-                                        loss=as.double(losses))
+                                         `_id` = 1:length(alpha_values),
+                                         alpha=as.double(alpha_values),
+                                         loss=as.double(losses),
+                                         nzero=as.numeric(nzeroes),
+                                         lambda=as.double(lambdas))
                                       
                                       
                                       output <- list("results" = results,
