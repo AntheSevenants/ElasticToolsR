55976bf532ae381294426d59bf08e279c7e195a8
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-04-04 15:00:23 +0200

    Use data frame for meta file by default

--- MetaFile.R
+++ MetaFile.R
@@ -1,21 +1,26 @@
 meta.file <- setRefClass("meta.file", fields = list(
-  triples = "list"),
+  df = "data.frame"),
   methods = list(
-    initialize = function() {
-      triples <<- list()
+    initialize = function(df_) {
+      columns <- c("subject", "predicate", "object") 
+      if (missing(df_)) {
+        df_ <- data.frame(matrix(nrow = 0, ncol = length(columns)))
+        colnames(df_) = columns
+      }
+      df <<- df_
     },
     
     add_model_information = function(predicate, object) {
       triple <- list(subject="model", predicate=predicate, object=object)
-      triples <<- append(triples, list(triple))
+      df <<- rbind(df, triple)
     },
     
     add_free_information = function(subject, predicate, object) {
       triple <- list(subject=subject, predicate=predicate, object=object)
-      triples <<- append(triples, list(triple))
+      df <<- rbind(df, triple)
     },
     
     as.data.frame = function() {
-      do.call(rbind, triples)
+      return(df)
     }
 ))
